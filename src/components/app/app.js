3c616af8ce4b45824218f6f1b29c30df83c1fc99
Viktor Kan <viktorkan9@gmail.com>
2019-05-06 10:06:44 +0300

    create search component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,6 +18,7 @@ export default class App extends Component {
       this.createTodoItem('Make awesone App'),
       this.createTodoItem('Have a lunch')
     ],
+    term: ''
   };
 
   createTodoItem(label) {
@@ -88,19 +89,41 @@ export default class App extends Component {
 
   };
 
+  search(items, term) {
+    if (term === '') {
+      return items
+    }
+
+    return items.filter((item) => {
+      return item.label.indexOf(term) > -1;
+    })
+  }
+
+  onSearch = (evt) => {
+   const {value} = evt.target;
+   this.setState(({term}) => {
+     return {
+       term: value
+     }
+   })
+  }
+
+
   render() {
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
+    const visibleItems = this.search(todoData, term);
+
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
     return (
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel onSearch={this.onSearch}/>
           <ItemStatusFilter />
         </div>
 
-        <TodoList todos={todoData}
+        <TodoList todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './search-panel.css';
 
-const SearchPanel = () => {
-  return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
-  );
+export default class SearchPanel extends Component {
+
+  render() {
+    const {onSearch} = this.props;
+    
+    return (
+      <input type="text"
+                className="form-control search-input"
+                placeholder="type to search" 
+                onChange = {onSearch}
+                />
+    );
+  }
 };
 
-export default SearchPanel;
+
+
